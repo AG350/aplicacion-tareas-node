a147cba97c05fe0a74a894534e3d73b38f507378
Alejandro Galeano <ale.gb32@gmail.com>
2020-06-22 22:23:35 -0300

    Completa funcionalidad crear tareas, filtrar por estados

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const tareas = require('./tareas');
 
-let accion = process.argv[2].toLowerCase;
+let accion = process.argv[2];
 let parametros = process.argv.slice(3);
+
+console.log(accion);
 
 switch (accion) {
     case undefined:
@@ -10,11 +12,15 @@ switch (accion) {
         tareas.listar();
         break;
 
-        case 'c':
-        case 'crear':
+    case 'c':
+    case 'crear':
         tareas.crear(parametros[0], parametros[1])
         break;
-        
+    
+    case 'le':
+    case 'estados':
+        tareas.listarPorEstado(parametros[0])
+        break;
     default:
         console.log();
         console.log('No entiendo esa acción');
--- tareas.js
+++ tareas.js
@@ -15,11 +15,39 @@ function escribirArchivoJSON(tareas) {
 }
 
 module.exports = { 
-    listar() {
+    listarPorEstado(estado) {
+        let tareasFiltradas = leerArchivoJSON().filter(elem => elem.estado == estado);
+        tareasFiltradas.forEach(tarea => {
+            console.log(tarea.titulo, '(' +  tarea.estado + ')');
+        });
+    },listar() {
         let tareas = leerArchivoJSON();
         
         tareas.forEach(tarea => {
-            console.log(tarea.titulo, '(' +  tarea.estado + ')');
+            console.log(tarea.titulo + ' '+ tarea.descripcion + ' (' +  tarea.estado + ')');
         });
+    },
+    crear(titulo = '', descripcion = '', estado = 'pendiente') {
+
+        if (titulo.length > 5) {
+    
+            let tareas = leerArchivoJSON();
+    
+            let tareaNueva = {
+                titulo: titulo,
+                descripcion: descripcion,
+                estado: estado
+            }
+            
+            tareas.push(tareaNueva);
+            
+            escribirArchivoJSON(tareas);
+            
+            console.log('¡Tarea creada con éxito!')
+    
+        } else {
+            console.log('Debes ingresar un título y debe tener al menos 5 caracteres');
+        }
+    
     }
 }
